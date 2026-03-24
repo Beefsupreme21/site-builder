7394e2cb090d4c02391c890830d78540cf19162b
Beefsupreme21 <beefsupreme21@hotmail.com>
2026-03-24 15:24:27 -0700

    add sites model, migration, and controller

---
+++ app/Models/Site.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+
+#[Fillable([
+    'slug',
+    'template',
+    'company_name',
+    'phone',
+    'email',
+    'logo',
+])]
+class Site extends Model {}
